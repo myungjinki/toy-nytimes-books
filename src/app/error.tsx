68b89775e72936d3306f753cca25ea42d812cfc1
Myung-jin Ki <myungjinki91@gmail.com>
2024-07-25 20:36:58 +0900

    Add nav, not-found, error page

---
+++ src/app/error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import styles from "../styles/not-found.module.css";
+
+export default function Error() {
+  return <div className={styles.div}>Error</div>;
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import styles from "../styles/not-found.module.css";
+
+export default function NotFound() {
+  return <div className={styles.div}>Not Found</div>;
+}
