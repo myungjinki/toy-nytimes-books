815fc79a26b9747829d43e73217465237d0c7346
Myungjin Ki <myungjinki91@gmail.com>
2024-11-15 16:12:58 +0900

    Modify List header

---
+++ app/components/backButton.tsx
@@ -0,0 +1,8 @@
+export default function BackButton() {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="text-2xl">{`<`}</div>
+      <div className="text-xl">{`Home`}</div>
+    </div>
+  );
+}
--- app/components/list-detail-header.tsx
+++ app/components/list-detail-header.tsx
@@ -1,3 +1,5 @@
+import BackButton from "./backButton";
+
 interface ListDetailHeaderProps {
   list_name: string;
 }
@@ -6,9 +8,11 @@ export default async function ListDetailHeader({
   list_name,
 }: ListDetailHeaderProps) {
   return (
-    <header className="relative flex flex-col justify-center gap-2 p-4 text-xl">
-      <div className="font-serif text-xl">{`< Home`}</div>
-      <div className="text-center">{list_name}</div>
+    <header className="relative flex flex-col justify-center gap-2 p-4">
+      <BackButton />
+      <div className="font-serif text-lg font-bold text-center ">
+        {list_name}
+      </div>
     </header>
   );
 }
